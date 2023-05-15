f04b78e7606bad6defd12fc8d515a33bbf46eba2
kmhbDOes <kmhbipul@gmail.com>
2023-05-15 21:45:53 +0600

    Pagination added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -12,25 +12,39 @@ import { Link, useLoaderData } from "react-router-dom";
 const Shop = () => {
   const [products, setProducts] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
   const [cart, setCart] = useState([]);
   const { totalProducts } = useLoaderData();
-  const itemsPerPage = 10; //TODO: Make it dynamic
+
   const totalPages = Math.ceil(totalProducts / itemsPerPage);
 
   const pageNumbers = [...Array(totalPages).keys()];
-  console.log(totalProducts);
+
   /**
    * Done: Determine total number of items
    * ToDo: Decide total number of items per page
    * Done: Calculate total pages
+   * Done: Determine current page
    *
    */
 
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/products")
+  //     .then((res) => res.json())
+  //     .then((data) => setProducts(data));
+  // }, []);
+
   useEffect(() => {
-    fetch("http://localhost:5000/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
+    async function fetchData() {
+      const response = await fetch(
+        `http://localhost:5000/products?page=${currentPage}&limit=${itemsPerPage}`
+      );
+
+      const data = await response.json();
+      setProducts(data);
+    }
+    fetchData();
+  }, [currentPage, itemsPerPage]);
 
   useEffect(() => {
     const storedCart = getShoppingCart();
@@ -52,6 +66,7 @@ const Shop = () => {
     setCart(savedCart);
   }, [products]);
 
+  const options = [5, 10, 20];
   const handleAddToCart = (product) => {
     // cart.push(product); '
     let newCart = [];
@@ -77,6 +92,11 @@ const Shop = () => {
     deleteShoppingCart();
   };
 
+  function handleSelectChange(event) {
+    setItemsPerPage(parseInt(event.target.value));
+    setCurrentPage(0);
+  }
+
   return (
     <>
       <div className="shop-container">
@@ -99,11 +119,27 @@ const Shop = () => {
       </div>
       {/* Pagination */}
       <div className="pagination">
+        <p>
+          current Page: {currentPage} and items per page: {itemsPerPage}
+        </p>
         {pageNumbers.map((number) => (
-          <button key={number} onClick={() => setCurrentPage(number)}>
+          <button
+            key={number}
+            className={currentPage === number ? "selected" : ""}
+            onClick={() => setCurrentPage(number)}
+          >
             {number}
           </button>
         ))}
+        {
+          <select value={itemsPerPage} onChange={handleSelectChange}>
+            {options.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        }
       </div>
     </>
   );
